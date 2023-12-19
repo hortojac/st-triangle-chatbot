d0d185a6305f0b8ac2c1e825c3f6e810ecb3c18a
Jacob Horton <jacob.horton@ku.edu>
2023-12-19 09:04:48 -0600

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,41 @@ from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
 from llama_index.llms import OpenAI
 import openai
 
-st.set_page_config(page_title="Chat with the Triangle ChatBot", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+# Set page configuration
+st.set_page_config(page_title="Chat with the Triangle ChatBot", page_icon="🔺", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+# Inject custom CSS with HTML
+st.markdown(f"""
+    <style>
+        header {{
+            background-color: #0e3745 !important;
+        }}
+        body {{
+            background-color: #808080;
+        }}
+        .stChatMessage {{
+            background-color: #990033;
+            border-radius: 10px;
+            padding: 10px;
+            margin: 5px;
+        }}
+        .stTextInput, .stButton, .stSpinner {{
+            color: #FFFFFF;
+        }}
+    </style>
+""", unsafe_allow_html=True)
+
+# API key and title
 openai.api_key = st.secrets.openai_key
 st.title("Chat with the Triangle ChatBot")
          
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+# Initialize chat messages history
+if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about the Kansas Chapter of Triangle Fraternity!"}
     ]
 
+# Load data function
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Triangle information – hang tight! This should take 1-2 minutes."):
@@ -23,21 +49,23 @@ def load_data():
 
 index = load_data()
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+# Initialize chat engine
+if "chat_engine" not in st.session_state.keys():
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+# User input and chat history
+if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
+for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# If last message is not from assistant, generate a new response
+# Generate new response from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+            st.session_state.messages.append(message)  # Add response to message history
