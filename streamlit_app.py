6debfaf058628ee4c4d1802da270614f6d16ff35
Jacob Horton <jacob.horton@ku.edu>
2023-12-21 20:47:05 -0600

    first doc conversion to txt file

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def load_data():
     with st.spinner(text="Loading and indexing the Triangle information – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Kansas Chapter of Triangle Fraternity and your job is to answer informative questions. Assume that all questions are related to the Kansas Chapter of Triangle. Keep your answers informative and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Kansas Chapter of Triangle Fraternity and the National Fraternity Organization. Your job is to answer informative questions. Assume that all questions are related to the Kansas Chapter of Triangle or Nationals. Keep your answers informative and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
